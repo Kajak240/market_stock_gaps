d3cf40b5e3cc9f1423fa099dab1f76079253a77a
kajak240 <bojo240@gmail.com>
2023-03-13 01:23:14 +0100

    added daily gap finding in single file

--- main.py
+++ main.py
@@ -5,25 +5,43 @@ import os
 import fnmatch
 import calendar
 
+def csvParser():
+
+    txt_dir_name = "wse_stocks/"
+
+    csv_dir_name = txt_dir_name + "csv/"
+
+    if not os.path.exists(csv_dir_name):
+        os.makedirs(csv_dir_name)
+
+    txt_files = fnmatch.filter(os.listdir(txt_dir_name), '*.txt') # tworzy zmienna z nazwami wszystkich plikow .txt
+    csv_files = fnmatch.filter(os.listdir(csv_dir_name), '*.csv') # tworzy zmienna z nazwami wszystkich plikow .csv
+
+    if len(csv_files) != len(txt_files):
+
+        csv_files = txt_files.copy()
+
+        delete_list = ["<", ">"]  # znaki do usuniecia z zawartosci pliku
+
+        for i in range(len(txt_files)):
+
+            csv_files[i] = txt_files[i].replace('.txt', '.csv')  # modyfikuje nazwe pliku w kopii z .txt na .csv
+
+            txt_files[i] = txt_dir_name + txt_files[i] # do kazdego elementu listy z nazwa pliku dodaje sciezke wzgledna (wse_stocks/nazwa_pliku.txt)
+            csv_files[i] = csv_dir_name + csv_files[i] # do kazdego elementu listy z nazwa pliku dodaje sciezke wzgledna (wse_stocks/nazwa_pliku.txt)
+
+            with open(txt_files[i]) as fin, open(csv_files[i], "w+") as fout: # petla przechodzi przez wszystkie pliki txt i tworzy pliki .csv
+                for line in fin:
+                    for word in delete_list:
+                        line = line.replace(word, "") #usuwa znaki do usuniecia z kazdego slowa w linijce w pliku
+                    fout.write(line) #zapisuje przetwarzana linijke do pliku .csv
+    else:
+        for i in range(len(csv_files)):
+            csv_files[i] = csv_dir_name + csv_files[i]  # do kazdego elementu listy z nazwa pliku dodaje sciezke wzgledna (wse_stocks/nazwa_pliku.txt)
+    return csv_files
+
+csv_files = csvParser()
 pd.options.display.max_rows = 9999 # pozwala na wyswietlanie duzej ilosci wartosci w outpucie (przy pracy na dataframesach)
-
-dir_name = "wse_stocks/"
-
-txt_files = fnmatch.filter(os.listdir(dir_name), '*.txt') # tworzy zmienna z nazwami wszystkich plikow .txt
-
-for i in range(len(txt_files)):
-    txt_files[i] = dir_name + txt_files[i] # do kazdego elementu listy z nazwa pliku dodaje sciezke wzgledna (wse_stocks/nazwa_pliku.txt)
-
-csv_files = txt_files.copy() # tworzy kopie 1:1 poprzedniej listy plikow
-
-for i in range(len(txt_files)):
-    csv_files[i] = txt_files[i].replace('.txt', '.csv') # modyfikuje nazwe pliku w kopii z .txt na .csv
-    delete_list = ["<", ">"] # znaki do usuniecia z zawartosci pliku
-    with open(txt_files[i]) as fin, open(csv_files[i], "w+") as fout: # petla przechodzi przez wszystkie pliki txt i tworzy pliki .csv
-        for line in fin:
-            for word in delete_list:
-                line = line.replace(word, "") #usuwa znaki do usuniecia z kazdego slowa w linijce w pliku
-            fout.write(line) #zapisuje przetwarzana linijke do pliku .csv
 
 # luki w trendzie spadkowym (zwiastujace przyszly wzrost)
 data = pd.read_csv(csv_files[0])
